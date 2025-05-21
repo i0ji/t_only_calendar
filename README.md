7dd02b533d6ab645a4a3712ffda569431fa14f4b
Ivan Vasilevich <ioji@inbox.ru>
2025-05-21 16:19:50 +0300

    feat(INTERFACE/APPEARANCE): main layout/markup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Test for Only:
 21.05.25:
 project init using webpack/react/sass/ts/git flow; repo init; first dev run; styling and preparigne env;
+main markup, style, set webpack;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import styles from './App.module.scss';
-console.log('вывв');
+import DateCarousel from './components/DateCarousel/DateCarousel';
+
+console.log('21.05.25/0.0.1');
 
 export default function App() {
-  return <div className={styles.container}>Hello world</div>;
+  return (
+    <section className={styles.main}>
+      <div className={styles.title}>
+        <hr />
+        <h1>
+          Исторические
+          <br /> даты
+        </h1>
+      </div>
+
+
+
+
+      <DateCarousel />
+      {/* <DateSlider/> */}
+    </section>
+  );
 }
